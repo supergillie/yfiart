53c7b4499b043bceab25ba1150d78ec809172e2c
pekr <peter.krooks@gmail.com>
2024-01-07 15:53:18 +0100

    fixed a default abba page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 import requests
 import random
 
@@ -6,7 +6,13 @@ app = Flask(__name__)
 
 BASE_URL = "https://api.discogs.com/database/search?type=master&artist="
 DISCOGS_URL = "https://api.discogs.com/database/search?type=master&artist=Santana"
-TOKEN = "qwzwnSYzsXKaYhvBPBgIMThxSCnGmRlgbAelZPsJ"
+TOKEN = "JLyawlHOYLPCOPyRQqVoASPqzwJAgRjldmGUNJwA"
+
+
+
+@app.route('/')
+def home():
+    return redirect('/artist/Abba', code=302)
 
 @app.route('/artist/<artist_name>')
 def artist(artist_name):
@@ -17,23 +23,13 @@ def artist(artist_name):
     }
     response = requests.get(url, headers=headers)
     data = response.json()
+    results_length = len(data['results'])  # Get the length of data['results']
 
+    print("Number of rows in data['results']:", len(data['results']))
+    
     # Randomly select 9 album covers if there are at least 9 results
     albums = random.sample(data['results'], min(9, len(data['results'])))
-    return render_template('index.html', albums=albums)
-
-@app.route('/')
-def index():
-    headers = {
-        "User-Agent": "FlaskApp/1.0 +http://localhost:5000",
-        "Authorization": f"Discogs token={TOKEN}"
-    }
-    response = requests.get(DISCOGS_URL, headers=headers)
-    data = response.json()
-
-    # Randomly select 9 album covers
-    albums = random.sample(data['results'], 9)
-    return render_template('index.html', albums=albums)
+    return render_template('index.html', albums=albums, results_length=results_length)
 
 if __name__ == '__main__':
     app.run(debug=True)
